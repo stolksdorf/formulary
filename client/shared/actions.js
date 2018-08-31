61f56820a8a86b2c9e5a2b8435429f4a3bb36f91
Scott Tolksdorf <scott.tolksdorf@gmail.com>
2018-08-31 02:43:03 -0400

    vitreum install

---
+++ client/shared/actions.js
@@ -0,0 +1,14 @@
+const Store = require('./store.js');
+const Actions = {
+	init : (initState)=>{
+		Store.setters.init(initState);
+	},
+	inc : (val = 1) => {
+		Actions.setVal(Store.getVal() + val);
+	},
+	setVal : (newVal) => {
+		Store.setters.setVal(newVal);
+	},
+};
+
+module.exports = Actions;
---
+++ config/config.init.js
@@ -0,0 +1,9 @@
+const exists = require('fs').existsSync;
+const env = process.env.NODE_ENV;
+
+const config = require('pico-conf')
+	.argv().env()
+	.defaults(require('./default.js'))
+
+if(exists(`./config/${env}.js`)) config.add(require(`./${env}.js`));
+module.exports = config;
